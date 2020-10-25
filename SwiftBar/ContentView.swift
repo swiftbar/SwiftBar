f0f68dc30156dd001344a622b7f4f4f05fde6fde
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-25 15:56:03 -0400

    Initial Commit

---
+++ SwiftBar/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftBar
+//
+//  Created by Alex on 10/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
