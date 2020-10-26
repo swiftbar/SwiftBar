4e15230296cb7b58de095e0b702d063a01106e21
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-26 19:26:26 -0400

    pretty print for Plugin protocol

---
+++ SwiftBar/App.swift
@@ -0,0 +1,38 @@
+import Cocoa
+
+class App: NSObject {
+    public static var pluginDirectoryPath: String? {
+        set {UserDefaults.standard.setValue(newValue, forKey: "PluginDirectoryPath")}
+        get {{UserDefaults.standard.string(forKey: "PluginDirectoryPath")}()}
+    }
+    public static func refreshPlugins() {
+        PluginManager.shared.loadPlugins()
+    }
+
+    public static func openPluginFolder() {
+        NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: "")
+    }
+
+    public static func changePluginFolder() {
+        let dialog = NSOpenPanel();
+
+        dialog.title                   = "Choose a plugin folder"
+        dialog.showsResizeIndicator    = true
+        dialog.showsHiddenFiles        = false
+        dialog.canChooseDirectories    = true
+        dialog.canCreateDirectories    = true
+        dialog.allowsMultipleSelection = false
+
+        guard dialog.runModal() == .OK,
+              let path = dialog.url?.path
+        else {return}
+
+        pluginDirectoryPath = path
+        refreshPlugins()
+    }
+
+    public static func getPlugins() {
+        let url = URL(string: "https://github.com/orgs/swiftbar/")!
+        NSWorkspace.shared.open(url)
+    }
+}
--- SwiftBar/Plugin/Plugin.swift
+++ SwiftBar/Plugin/Plugin.swift
@@ -12,8 +12,28 @@ protocol Plugin {
     var type: PluginType { get }
     var name: String { get }
     var file: String { get }
-    var metadata: PluginMetadata { get }
+    var metadata: PluginMetadata? { get }
     func refresh()
     func terminate()
     func invoke(params: [String])
+}
+
+extension Plugin {
+    var description: String {
+        return """
+        id: \(id)
+        type: \(type)
+        name: \(name)
+        path: \(file)
+        metadata:
+            name: \(metadata?.name ?? "")
+            author: \(metadata?.author ?? "")
+            version: \(metadata?.version ?? "")
+            github: \(metadata?.github ?? "")
+            desc: \(metadata?.desc ?? "")
+            previewImageURL: \(metadata?.previewImageURL ?? "")
+            dependencies: \(metadata?.dependencies.joined(separator: ",") ?? "")
+            aboutURL: \(metadata?.aboutURL?.absoluteString ?? "")
+        """
+    }
 }
