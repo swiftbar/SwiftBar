75feb8922eaf13f3242437167cdaaea5d0a5fd64
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-31 11:46:39 -0400

    URL Scheme

--- SwiftBar/AppDelegate.swift
+++ SwiftBar/AppDelegate.swift
@@ -8,9 +8,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func application(_ application: NSApplication, open urls: [URL]) {
+        for url in urls {
+            switch url.host?.lowercased() {
+                case "refreshallplugins":
+                    pluginManager.refreshAllPlugins()
+                case "refreshplugin":
+                    if let name = url.queryParameters?["name"] {
+                        pluginManager.refreshPlugin(named: name)
+                        return
+                    }
+                    if let indexStr = url.queryParameters?["index"], let index = Int(indexStr) {
+                        pluginManager.refreshPlugin(with: index)
+                        return
+                    }
+                case "addplugin":
+                    if let src = url.queryParameters?["src"], let url = URL(string: src) {
+                        pluginManager.importPlugin(from: url)
+                    }
+                default:
+                    break
+            }
+        }
     }
-
 }
-
---
+++ SwiftBar/Utility/URL+Extension.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension URL {
+    public var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems else { return nil }
+        return queryItems.reduce(into: [String: String]()) { (result, item) in
+            result[item.name] = item.value
+        }
+    }
+}
