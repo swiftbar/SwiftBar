fda54f6f4d96bb5bfb028476635db5c02ff148f1
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-11-02 15:28:08 -0500

    fixed NSColor from hex

--- SwiftBar/Utility/NSColor.swift
+++ SwiftBar/Utility/NSColor.swift
@@ -9,34 +9,50 @@ extension NSColor {
         guard let colorString = colorString else {return nil}
 
         if colorString.hasPrefix("#") {
-            return colorWithHexColorString(colorString: colorString)
+            return fromHexString(hex: colorString)
         }
 
         if let color = NSColor.cssColors[colorString] {
-            return colorWithHexColorString(colorString: color)
+            return fromHexString(hex: color)
         }
 
         return nil
     }
 
-    static func colorWithHexColorString(colorString: String) -> NSColor? {
-        let colorString = colorString.hasPrefix("#") ? String(colorString.dropFirst()):colorString
-
-        var color: NSColor? = nil
-        var colorCode = UInt32()
-        var redByte:CGFloat = 255;
-        var greenByte:CGFloat = 255;
-        var blueByte: CGFloat = 255;
-
-        let scanner = Scanner(string: colorString)
-        if scanner.scanHexInt32(&colorCode) {
-            redByte = CGFloat(colorCode & 0xff0000)
-            greenByte = CGFloat(colorCode & 0x00ff00)
-            blueByte =  CGFloat(colorCode & 0xff)
-            color = NSColor(red: redByte, green: greenByte, blue: blueByte, alpha: 1.0)
-        }
-
-        return color
+    class func fromHex(hex: Int, alpha: Float) -> NSColor {
+        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
+        let blue = CGFloat((hex & 0xFF)) / 255.0
+        return NSColor(calibratedRed: red, green: green, blue: blue, alpha: 1.0)
     }
 
+    class func fromHexString(hex: String, alpha: Float = 1) -> NSColor? {
+        var cleanedString = hex
+        if hex.hasPrefix("0x") {
+            cleanedString = String(hex.dropFirst(2))
+        } else if hex.hasPrefix("#") {
+            cleanedString = String(hex.dropFirst())
+        }
+        // Ensure it only contains valid hex characters 0
+        let validHexPattern = "[a-fA-F0-9]+"
+        if cleanedString.conformsTo(pattern: validHexPattern) {
+            var theInt: UInt32 = 0
+            let scanner = Scanner(string: cleanedString)
+            scanner.scanHexInt32(&theInt)
+            let red = CGFloat((theInt & 0xFF0000) >> 16) / 255.0
+            let green = CGFloat((theInt & 0xFF00) >> 8) / 255.0
+            let blue = CGFloat((theInt & 0xFF)) / 255.0
+            return NSColor(calibratedRed: red, green: green, blue: blue, alpha: 1.0)
+
+        } else {
+            return nil
+        }
+    }
+}
+
+extension String  {
+    func conformsTo(pattern: String) -> Bool {
+        let pattern = NSPredicate(format:"SELF MATCHES %@", pattern)
+        return pattern.evaluate(with: self)
+    }
 }
