320eb30193e2567b9b475b2527f9b10369a860c0
Alex Mazanov <alexandr.mazanov@gmail.com>
2020-12-10 21:37:51 -0500

    Adjusted env variables naming #50

--- SwiftBar/Utility/RunScript.swift
+++ SwiftBar/Utility/RunScript.swift
@@ -2,9 +2,9 @@ import Foundation
 import ShellOut
 
 fileprivate let systemEnv: [String:String] = [
-    "SwiftBar":"1",
-    "SwiftBarVersion": (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""),
-    "SwiftBarBuild": (Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "")
+    "SWIFTBAR":"1",
+    "SWIFTBAR_VERSION": (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""),
+    "SWIFTBAR_BUILD": (Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "")
 ]
 
 @discardableResult func runScript(to command: String, env: [String:String] = [:]) throws -> String {
