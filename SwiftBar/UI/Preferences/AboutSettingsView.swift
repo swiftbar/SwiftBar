14f508dc19008cdd2122f8029f37640944e602d7
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-11-05 10:41:17 -0400

    Add About Settings View

---
+++ SwiftBar/UI/Preferences/AboutSettingsView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct AboutSettingsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(nsImage: NSImage(named: "AppIcon")!)
+                    .resizable()
+                    .renderingMode(.original)
+                    .frame(width: 90, height: 90, alignment: .leading)
+
+                VStack(alignment: .leading) {
+                    if #available(macOS 11.0, *) {
+                        Text("SwiftBar")
+                            .font(.title3)
+                            .bold()
+                    }
+                    Text("Version \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "") (\(Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""))")
+                        .font(.subheadline)
+                    Text("Copyright ©2020-2022 Ameba Labs. All rights reserved.")
+                        .font(.footnote)
+                        .padding(.top, 10)
+                }
+            }
+            Spacer()
+            Divider()
+            HStack {
+                Spacer()
+                Button("Visit our Website", action: {
+                    NSWorkspace.shared.open(URL(string: "https://swiftbar.app")!)
+                })
+                Button("Contact Us", action: {
+                    NSWorkspace.shared.open(URL(string: "mailto:info@swiftbar.app")!)
+                })
+            }.padding(.top, 10)
+                .padding(.bottom, 10)
+        }.padding(10)
+            .frame(width: 410, height: 160)
+    }
+}
